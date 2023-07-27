18c6811a958cc044136ed4556c3595ab0a39c3b3
thuvu17 <112644941+thuvu17@users.noreply.github.com>
2023-07-27 17:18:34 -0400

    Update photosearch.js

--- frontend/assets/js/photosearch.js
+++ frontend/assets/js/photosearch.js
@@ -1,17 +1,35 @@
-document.getElementById("getKeywordForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent the form from submitting the traditional way
+// GET keyword
 
-  // Get the search keyword from the input field
+document.getElementById("getKeywordForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the form from submitting via HTTP
+
+  // Get form value
   var keyword = document.getElementById("keyword").value;
 
-  // Make the API request using the SDK's method (assuming the SDK has a `searchPhotoAlbums` method)
-  sdk.searchPhotoAlbums({ keyword: keyword })
-    .then(function(response) {
-      // Request succeeded, handle the response data here
-      console.log(response.data); // Process the response data as needed
-    })
-    .catch(function(error) {
-      // Request failed, handle the error here
-      console.error("Error:", error);
-    });
+  // Do something with the form data
+  console.log(keyword);
+  return sdk.searchGet({q: keyword}, {}, {});
 });
+
+
+// PUT photo
+
+document.getElementById('uploadImageForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  // Get image input
+  var imageFile = document.getElementById('imageInput').files[0];
+
+  // Get the custom labels 
+  var customLabels = document.getElementById('customLabels').value.split(',');
+
+  // Create a FormData object to send the file and custom labels
+  var formData = new FormData();
+  formData.append('image', imageFile);
+  formData.append('customLabels', JSON.stringify(customLabels));
+
+  uploadImage(formData);
+});
+
+function uploadImage(file) {
+  return sdk.uploadPut();
+}
