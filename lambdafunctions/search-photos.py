2d29408f62212a44aa78197b5cd3d85f21b6ac08
yifei-yao <yaoyifei0@gmail.com>
2023-07-30 06:12:11 -0400

    test query

--- lambdafunctions/search-photos.py
+++ lambdafunctions/search-photos.py
@@ -1,68 +1,78 @@
-import boto3
-import requests
-from aws_requests_auth.aws_auth import AWSRequestsAuth
+# import boto3
+# import requests
+# from aws_requests_auth.aws_auth import AWSRequestsAuth
 
 
 def lambda_handler(event, context):
-    # Extract the user input text from the input event
-    user_input = "show me dogs"
+    query_param = event.get("queryStringParameters", {}).get("q")
 
-    # Lex bot
-    lex = boto3.client("lexv2-runtime", region_name="us-east-1")
+    if query_param:
+        return {
+            "statusCode": 200,
+            "body": f'The query parameter "q" was "{query_param}"',
+        }
+    else:
+        return {"statusCode": 400, "body": 'No query parameter "q" provided'}
 
-    # Invoke the Lex v2 bot with the user input
-    bot_id = "4G2QTRTNLS"
-    bot_alias_id = "TSTALIASID"
-    locale_id = "en_US"
+    # # Extract the user input text from the input event
+    # user_input = "show me dogs"
 
-    response = lex.recognize_text(
-        botId=bot_id,
-        botAliasId=bot_alias_id,
-        localeId=locale_id,
-        sessionId="abc1234",
-        text=user_input,
-    )
+    # # Lex bot
+    # lex = boto3.client("lexv2-runtime", region_name="us-east-1")
 
-    if "sessionState" in response:
-        # Get keywords
-        message = response["sessionState"]["intent"]["slots"]["Keyword"]["values"]
-        keywords = []
-        match_query = []
-        for i in range(len(message)):
-            keywords.append(message[i]["value"]["interpretedValue"])
-            match_query.append({"term": {"labels": keywords[i]}})
-        # return match_query
+    # # Invoke the Lex v2 bot with the user input
+    # bot_id = "4G2QTRTNLS"
+    # bot_alias_id = "TSTALIASID"
+    # locale_id = "en_US"
 
-        # Query ElasticSearch
-        index = "photos"
-        host = "search-photos1-hwrbp5mxflgqrrzwku2dllowjm.us-east-1.es.amazonaws.com"
-        url = "https://" + host + "/" + index + "/_search/"
+    # response = lex.recognize_text(
+    #     botId=bot_id,
+    #     botAliasId=bot_alias_id,
+    #     localeId=locale_id,
+    #     sessionId="abc1234",
+    #     text=user_input,
+    # )
 
-        headers = {"Content-Type": "application/json"}
-        region = "us-east-1"
-        service = "es"
+    # if "sessionState" in response:
+    #     # Get keywords
+    #     message = response["sessionState"]["intent"]["slots"]["Keyword"]["values"]
+    #     keywords = []
+    #     match_query = []
+    #     for i in range(len(message)):
+    #         keywords.append(message[i]["value"]["interpretedValue"])
+    #         match_query.append({"term": {"labels": keywords[i]}})
+    #     # return match_query
 
-        session = boto3.Session()
-        credentials = session.get_credentials()
+    #     # Query ElasticSearch
+    #     index = "photos"
+    #     host = "search-photos1-hwrbp5mxflgqrrzwku2dllowjm.us-east-1.es.amazonaws.com"
+    #     url = "https://" + host + "/" + index + "/_search/"
 
-        auth = AWSRequestsAuth(
-            aws_access_key=credentials.access_key,
-            aws_secret_access_key=credentials.secret_key,
-            aws_host=host,
-            aws_region=region,
-            aws_service=service,
-            aws_token=credentials.token,
-        )
+    #     headers = {"Content-Type": "application/json"}
+    #     region = "us-east-1"
+    #     service = "es"
 
-        query = {"query": {"bool": {"should": match_query, "minimum_should_match": 1}}}
+    #     session = boto3.Session()
+    #     credentials = session.get_credentials()
 
-        headers = {"Content-Type": "application/json"}
+    #     auth = AWSRequestsAuth(
+    #         aws_access_key=credentials.access_key,
+    #         aws_secret_access_key=credentials.secret_key,
+    #         aws_host=host,
+    #         aws_region=region,
+    #         aws_service=service,
+    #         aws_token=credentials.token,
+    #     )
 
-        response = requests.get(url, auth=auth, headers=headers, json=query)
-        return response.json()
-        hits = response.json()["hits"]["hits"]
-        return_photos = [hit["_source"]["keyword"] for hit in hits]
+    #     query = {"query": {"bool": {"should": match_query, "minimum_should_match": 1}}}
 
-        return return_photos
+    #     headers = {"Content-Type": "application/json"}
 
-    return {"statusCode": 500, "body": "No response from Lex!"}
+    #     response = requests.get(url, auth=auth, headers=headers, json=query)
+    #     return response.json()
+    #     hits = response.json()["hits"]["hits"]
+    #     return_photos = [hit["_source"]["keyword"] for hit in hits]
+
+    #     return return_photos
+
+    # return {"statusCode": 500, "body": "No response from Lex!"}
