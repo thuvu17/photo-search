d5a96d14d0c9c70e24ccaf929e19c3084f3f6270
thuvu17 <112644941+thuvu17@users.noreply.github.com>
2023-08-01 17:56:06 -0400

    testing GET (PUT is commented)

--- frontend/assets/js/photosearch.js
+++ frontend/assets/js/photosearch.js
@@ -12,24 +12,62 @@ document.getElementById("getKeywordForm").addEventListener("submit", function(ev
 });
 
 
+  // Add an event listener to the submit button
+  document.getElementById('uploadImageForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var file = document.getElementById('imageInput').files[0];
+      var customLabels = document.getElementById("customLabels");
+      console.log(customLabels.value);
+      var reader = new FileReader();
+
+      reader.onload = function (event) {//
+          // var body = btoa(event.target.result.replace(/^data:(.*;base64,)?/, ''));
+          var body = event.target.result;
+          // console.log(body);
+          // console.log(file.type);
+          var filename = Date.now() + file.name
+          // console.log(filename)
+          var params = {
+              'bucket': 'new-b2-bucket',
+              'filename': filename,
+              'Content-Type': file.type,
+              'x-amz-meta-customLabels': customLabels.value,
+          };
+          sdk.uploadPut(params, body, {})
+              .then(function (res) {
+                  if (res.status == 200) {
+                      alert(filename, "IMAGE UPLOADED!!")
+                  }
+              });
+      }
+      reader.readAsDataURL(file);
+  }, false);
+
 // PUT photo
 
-document.getElementById('uploadImageForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-  // Get image input
-  var imageFile = document.getElementById('imageInput').files[0];
+// document.getElementById('uploadImageForm').addEventListener('submit', function (event) {
+//   event.preventDefault();
+//   // Get image input
+//   var imageFile = document.getElementById('imageInput').files[0];
 
-  // Get the custom labels 
-  var customLabels = document.getElementById('customLabels').value.split(',');
+//   // Get the custom labels 
+//   var customLabels = document.getElementById('customLabels').value.split(',');
+//   console.log(customLabels);
+//   // Create a FormData object to send the file and custom labels
+//   var formData = new FormData();
+//   formData.append('image', imageFile);
+//   formData.append('customLabels', JSON.stringify(customLabels));
 
-  // Create a FormData object to send the file and custom labels
-  var formData = new FormData();
-  formData.append('image', imageFile);
-  formData.append('customLabels', JSON.stringify(customLabels));
+//   return sdk.uploadPut(formData)
+// });
 
-  uploadImage(formData);
+document.getElementById("getKeywordForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the form from submitting via HTTP
+
+  // Get form value
+  var keyword = document.getElementById("keyword").value;
+
+  // Do something with the form data
+  console.log(keyword);
+  return sdk.searchGet({q: keyword}, {}, {});
 });
-
-function uploadImage(file) {
-  return sdk.uploadPut();
-}
